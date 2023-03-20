33205a8e8a399781943fb923ca1d47f57dc75b07
marsdong <marsdong@tencent.com>
2023-03-20 18:01:54 +0800

    fix bug for used percent

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module main
 go 1.16
 
 require (
+	github.com/containerd/cgroups v1.1.0
 	github.com/dustin/go-humanize v1.0.0
 	github.com/shirou/gopsutil v3.21.11+incompatible
-	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	golang.org/x/sys v0.0.0-20211209171907-798191bca915 // indirect
 )
--- main.go
+++ main.go
@@ -24,22 +24,29 @@ import (
 	"syscall"
 	"time"
 
-	humanize "github.com/dustin/go-humanize"
+	"github.com/containerd/cgroups"
+	v1 "github.com/containerd/cgroups/stats/v1"
+	"github.com/dustin/go-humanize"
 	psutil "github.com/shirou/gopsutil/mem"
 )
 
 var (
-	memSize    string
-	growthTime string
-	workers    int
-	client     bool
+	requireCgroupLimit bool
+	pidNum             int
+	memSize            string
+	growthTime         string
+	workers            int
+	client             bool
 )
 
 func init() {
 	flag.StringVar(&memSize, "size", "0KB", "size of memory you want to allocate")
+	flag.IntVar(&pidNum, "pid", 0, "container pid numer")
 	flag.StringVar(&growthTime, "time", "0s", "time to reach the size of memory you allocated")
 	flag.IntVar(&workers, "workers", 1, "number of workers allocating memory")
 	flag.BoolVar(&client, "client", false, "the process runs as a client")
+	flag.BoolVar(&requireCgroupLimit, "required-limit", true, "required container has "+
+		"resource limit")
 	flag.Parse()
 }
 
@@ -56,7 +63,8 @@ func linearGrow(data []byte, length uint64, timeLine time.Duration) {
 		if now.After(endTime) {
 			now = endTime
 		}
-		expected := length * uint64(now.Sub(startTime).Milliseconds()) / uint64(endTime.Sub(startTime).Milliseconds()) / pageSize
+		expected := length * uint64(now.Sub(startTime).Milliseconds()) /
+			uint64(endTime.Sub(startTime).Milliseconds()) / pageSize
 
 		for i := allocated; uint64(i) < expected; i++ {
 			data[uint64(i)*pageSize] = 0
@@ -73,7 +81,8 @@ func linearGrow(data []byte, length uint64, timeLine time.Duration) {
 }
 
 func run(length uint64, timeLine time.Duration) {
-	data, err := syscall.Mmap(-1, 0, int(length), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_PRIVATE|syscall.MAP_ANONYMOUS)
+	data, err := syscall.Mmap(-1, 0, int(length), syscall.PROT_READ|syscall.PROT_WRITE,
+		syscall.MAP_PRIVATE|syscall.MAP_ANONYMOUS)
 	if err != nil {
 		// TODO
 		fmt.Println(err)
@@ -100,42 +109,140 @@ func main() {
 		for {
 			workQueue <- struct{}{}
 			go func() {
-				cmd := exec.Command("memStress", "--size", memSize, "--workers", fmt.Sprintf("%d", workers),
-					"--time", growthTime, "--client", "1")
+				args := []string{
+					"--size", memSize,
+					"--workers", fmt.Sprintf("%d", workers),
+					"--time", growthTime,
+					"--client", "false",
+					"--pid", fmt.Sprintf("%d", pidNum),
+				}
+
+				if !requireCgroupLimit {
+					args = append(args, "--required-limit")
+					args = append(args, "false")
+				}
+
+				cmd := exec.Command("memStress", args...)
 				cmd.SysProcAttr = &syscall.SysProcAttr{
 					Pdeathsig: syscall.SIGTERM,
 				}
 				err := cmd.Run()
 				if err != nil {
-					fmt.Println(err)
+					exitWithError(err)
 				}
 				<-workQueue
 			}()
 			time.Sleep(time.Second)
 		}
 	} else {
-		memInfo, _ := psutil.VirtualMemory()
-		var length uint64
+		percentage, expectBytes, err := parseSize(memSize)
+		fmt.Fprintf(os.Stdout, "percentage: %.2f, expectBytes: %d\n", percentage, expectBytes)
+		if err != nil {
+			exitWithError(err)
+		}
 
-		if memSize[len(memSize)-1] != '%' {
-			var err error
-			length, err = humanize.ParseBytes(memSize)
-			if err != nil {
-				// TODO
-				fmt.Println(err)
-			}
-		} else {
-			percentage, err := strconv.ParseFloat(memSize[0:len(memSize)-1], 64)
-			if err != nil {
-				fmt.Println(err)
-			}
-			length = uint64(float64(memInfo.Total) / 100.0 * percentage)
+		filledSize, err := calculateMemSize(percentage, expectBytes)
+		if err != nil {
+			exitWithError(err)
 		}
 
 		timeLine, err := time.ParseDuration(growthTime)
 		if err != nil {
-			// TODO
+			fmt.Println(err)
 		}
-		run(length/uint64(workers), timeLine)
+		run(filledSize/uint64(workers), timeLine)
 	}
 }
+
+func exitWithError(err error) {
+	_, _ = fmt.Fprintf(os.Stderr, err.Error())
+	os.Exit(1)
+}
+
+// parseSize parse size
+// Two size formats are supported, which are 100MB or 30%
+// @Author MarsDong 2023-03-20 17:08:11
+func parseSize(memSize string) (float64, uint64, error) {
+	if memSize[len(memSize)-1] != '%' {
+		length, err := humanize.ParseBytes(memSize)
+		return 0, length, err
+	}
+	percentage, err := strconv.ParseFloat(memSize[0:len(memSize)-1], 64)
+	return percentage, 0, err
+}
+
+// calculateMemSize get actual filled size
+// If the user specifies the size of memory to fill, it fills directly, and if a percentage is specified,
+// the remaining size to fill needs to be calculated based on the current memory usage.
+// @Author MarsDong 2023-03-20 16:11:00
+func calculateMemSize(percent float64, expectUsed uint64) (uint64, error) {
+	if expectUsed > 0 {
+		return expectUsed, nil
+	}
+
+	nodeMemory, err := getMemFromNode()
+	if err != nil {
+		return 0, err
+	}
+	cgroupMemory, err := getMemFromCgroup(pidNum)
+	if err != nil {
+		return 0, err
+	}
+
+	total := nodeMemory.Total
+	used := nodeMemory.Used
+
+	// an error is returned if the container does not specify a resource limit
+	if cgroupMemory.Usage.Limit < nodeMemory.Total {
+		total = cgroupMemory.Usage.Limit
+		used = cgroupMemory.Usage.Usage
+	} else if requireCgroupLimit {
+		return 0, fmt.Errorf("require resource limit")
+	}
+
+	currentUsedPercent, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", float64(used)/float64(total)), 64)
+	if currentUsedPercent > percent {
+		return 0, fmt.Errorf("current used percent %.2f larger than expect %.2f", currentUsedPercent, percent)
+	}
+
+	actualFilled := uint64(((percent - currentUsedPercent) / 100.0) * float64(total))
+	fmt.Fprintf(os.Stdout, "total: %dKB, used: %dKB, actualFilled: %dKB\n", total, used, actualFilled)
+	return actualFilled, nil
+}
+
+// getMemFromNode get total memory from k8s node
+// @Author MarsDong 2023-03-20 16:10:35
+func getMemFromNode() (*psutil.VirtualMemoryStat, error) {
+	memInfo, err := psutil.VirtualMemory()
+	if err != nil {
+		return nil, err
+	}
+	return memInfo, nil
+}
+
+// getMemFromCgroup get memory limit from cgroup configuration
+// @Author MarsDong 2023-03-20 16:10:14
+func getMemFromCgroup(pidNum int) (*v1.MemoryStat, error) {
+	pathMap, err := cgroups.ParseCgroupFile(fmt.Sprintf("/proc/%d/cgroup", pidNum))
+	if err != nil {
+		return nil, err
+	}
+
+	memRoot, exists := pathMap["memory"]
+	if !exists {
+		return nil, fmt.Errorf("not found root path for memory")
+	}
+
+	cgroupIns, err := cgroups.Load(cgroups.V1, cgroups.StaticPath(memRoot))
+	if err != nil {
+		return nil, err
+	}
+
+	stats, err := cgroupIns.Stat(cgroups.IgnoreNotExist)
+	if err != nil {
+		return nil, err
+	}
+
+	return stats.Memory, nil
+}
+
